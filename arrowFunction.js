4c70d9893f2033133412ecec5af5b18cde54e35d
RanjithKumar <ranjithkumarpatil@gmail.com>
2022-09-02 22:36:50 +0530

    arrow function

---
+++ arrowFunction.js
@@ -0,0 +1,12 @@
+// const myFunc = (name) => {
+//   console.log(name);
+// };
+// myFunc("Ranjith");
+
+// const myName = () => {
+//   console.log("Ranjith");
+// };
+// myName();
+
+const multipy = (num) => num * 2;
+console.log(multipy(2));
